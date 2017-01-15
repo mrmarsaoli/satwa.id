609b85e1e27caaca5234abea6107ab2ed90643ee
prasetyotanjungm <muhammad8035@students.amikom.ac.id>
2017-01-15 21:31:37 +0700

    DATABAZEEE
    Yanng ini gan yang asli gara gara lek ini

---
+++ db_satwa (1).sql
@@ -0,0 +1,449 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15 Jan 2017 pada 15.30
+-- Versi Server: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_satwa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_tiket`
+--
+
+CREATE TABLE IF NOT EXISTS `detail_tiket` (
+  `id_detail_tiket` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `id_pesan_tiket` int(11) NOT NULL,
+  `id_event` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Trigger `detail_tiket`
+--
+DELIMITER $$
+CREATE TRIGGER `tiket_dipesan` AFTER INSERT ON `detail_tiket`
+ FOR EACH ROW BEGIN 
+UPDATE event SET tiket_tersedia=tiket_tersedia-NEW.jumlah
+WHERE id_event = NEW.id_event;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `event`
+--
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `id_event` int(11) NOT NULL,
+  `nama_event` varchar(255) NOT NULL,
+  `deskripsi event` varchar(255) NOT NULL,
+  `Lokasi` varchar(255) NOT NULL,
+  `Contact Person` varchar(20) NOT NULL,
+  `Hadiah` varchar(255) NOT NULL,
+  `tanggal_event` datetime NOT NULL,
+  `tanggal_publish` datetime DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_sub_kategori` int(11) NOT NULL,
+  `id_owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Trigger `event`
+--
+DELIMITER $$
+CREATE TRIGGER `tg_date_event` BEFORE INSERT ON `event`
+ FOR EACH ROW INSERT INTO event(`tanggal_publish`) VALUES(NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hewan`
+--
+
+CREATE TABLE IF NOT EXISTS `hewan` (
+  `id_hewan` int(11) NOT NULL,
+  `nama_hewan` varchar(255) DEFAULT NULL,
+  `harga` int(11) NOT NULL,
+  `cek` enum('sudah diadopsi','belum diadopsi') NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `jenis_hewan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kritik`
+--
+
+CREATE TABLE IF NOT EXISTS `kritik` (
+  `id_kritik` int(11) NOT NULL,
+  `isi` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `id_admin` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `owner`
+--
+
+CREATE TABLE IF NOT EXISTS `owner` (
+  `id_owner` int(11) NOT NULL,
+  `nama_owner` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengumuman`
+--
+
+CREATE TABLE IF NOT EXISTS `pengumuman` (
+  `id_pengumuman` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `isi` text NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesan_tiket`
+--
+
+CREATE TABLE IF NOT EXISTS `pesan_tiket` (
+  `id_pesan_tiket` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `tanggal_pesan` datetime NOT NULL,
+  `status` enum('lunas','belum lunas') DEFAULT NULL,
+  `tanggal_update` datetime DEFAULT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sub_kategori`
+--
+
+CREATE TABLE IF NOT EXISTS `sub_kategori` (
+  `id_sub_kategori` int(11) NOT NULL,
+  `nama_sub_kategori` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `tiket_tersedia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `super_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `super_admin` (
+  `id_super_admin` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `trans_beli`
+--
+
+CREATE TABLE IF NOT EXISTS `trans_beli` (
+  `id_trans_beli` int(11) NOT NULL,
+  `tanggal_beli` int(11) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `tanggal_update` datetime NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_hewan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jabatan` varchar(255) DEFAULT NULL,
+  `no_telp` varchar(20) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `detail_tiket`
+--
+ALTER TABLE `detail_tiket`
+  ADD PRIMARY KEY (`id_detail_tiket`),
+  ADD KEY `id_pesan_tiket` (`id_pesan_tiket`),
+  ADD KEY `id_event` (`id_event`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id_event`),
+  ADD KEY `id_user` (`id_user`,`id_kategori`),
+  ADD KEY `id_user_2` (`id_user`,`id_kategori`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_sub_kategori` (`id_sub_kategori`),
+  ADD KEY `id_owner` (`id_owner`);
+
+--
+-- Indexes for table `hewan`
+--
+ALTER TABLE `hewan`
+  ADD PRIMARY KEY (`id_hewan`),
+  ADD KEY `id_kategori` (`id_kategori`,`id_user`),
+  ADD KEY `id_kategori_2` (`id_kategori`,`id_user`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `kritik`
+--
+ALTER TABLE `kritik`
+  ADD PRIMARY KEY (`id_kritik`),
+  ADD KEY `id_user` (`id_admin`);
+
+--
+-- Indexes for table `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`id_owner`);
+
+--
+-- Indexes for table `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD PRIMARY KEY (`id_pengumuman`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `pesan_tiket`
+--
+ALTER TABLE `pesan_tiket`
+  ADD PRIMARY KEY (`id_pesan_tiket`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_user_2` (`id_user`);
+
+--
+-- Indexes for table `sub_kategori`
+--
+ALTER TABLE `sub_kategori`
+  ADD PRIMARY KEY (`id_sub_kategori`);
+
+--
+-- Indexes for table `super_admin`
+--
+ALTER TABLE `super_admin`
+  ADD PRIMARY KEY (`id_super_admin`);
+
+--
+-- Indexes for table `trans_beli`
+--
+ALTER TABLE `trans_beli`
+  ADD PRIMARY KEY (`id_trans_beli`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_hewan` (`id_hewan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `detail_tiket`
+--
+ALTER TABLE `detail_tiket`
+  MODIFY `id_detail_tiket` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `hewan`
+--
+ALTER TABLE `hewan`
+  MODIFY `id_hewan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kritik`
+--
+ALTER TABLE `kritik`
+  MODIFY `id_kritik` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `id_owner` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pesan_tiket`
+--
+ALTER TABLE `pesan_tiket`
+  MODIFY `id_pesan_tiket` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sub_kategori`
+--
+ALTER TABLE `sub_kategori`
+  MODIFY `id_sub_kategori` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `super_admin`
+--
+ALTER TABLE `super_admin`
+  MODIFY `id_super_admin` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detail_tiket`
+--
+ALTER TABLE `detail_tiket`
+  ADD CONSTRAINT `detail_tiket_ibfk_1` FOREIGN KEY (`id_pesan_tiket`) REFERENCES `pesan_tiket` (`id_pesan_tiket`),
+  ADD CONSTRAINT `fk_detail_event` FOREIGN KEY (`id_event`) REFERENCES `event` (`id_event`);
+
+--
+-- Ketidakleluasaan untuk tabel `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `event_ibfk_3` FOREIGN KEY (`id_sub_kategori`) REFERENCES `sub_kategori` (`id_sub_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `event_ibfk_4` FOREIGN KEY (`id_owner`) REFERENCES `owner` (`id_owner`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `event_ibfk_5` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `hewan`
+--
+ALTER TABLE `hewan`
+  ADD CONSTRAINT `hewan_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `hewan_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `kritik`
+--
+ALTER TABLE `kritik`
+  ADD CONSTRAINT `kritik_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD CONSTRAINT `pengumuman_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pesan_tiket`
+--
+ALTER TABLE `pesan_tiket`
+  ADD CONSTRAINT `pesan_tiket_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `trans_beli`
+--
+ALTER TABLE `trans_beli`
+  ADD CONSTRAINT `trans_beli_ibfk_2` FOREIGN KEY (`id_hewan`) REFERENCES `hewan` (`id_hewan`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `trans_beli_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
